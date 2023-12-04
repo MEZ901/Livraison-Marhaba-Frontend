156082c0de74e1481e1e30614573e14eec95ed69
abrache <abrache2001@gmail.com>
2023-12-04 21:29:27 +0100

    adding style

--- src/shared/components/Heder.jsx
+++ src/shared/components/Heder.jsx
@@ -85,6 +85,7 @@ function Header() {
               </Dropdown>
             </NavbarContent>
           </>
+          
         ) : (
           <NavbarContent justify="end">
             <NavbarItem>
