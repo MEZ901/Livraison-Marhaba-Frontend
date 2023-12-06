b09e7845e97fedfd0fc04dfd38043ecccc9a742e
Ielmarzouqy <elmarzouqyimane@gmail.com>
2023-12-06 18:09:08 +0100

    add order

--- README.md
+++ README.md
@@ -6,3 +6,65 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+
+## Getting Started
+
+These instructions will help you set up and run the Livraison Marhaba Frontend on your local machine for development.
+
+### 1. Installation
+
+#### 1.1. Clone the repository
+
+
+```sh
+https://github.com/Ielmarzouqy/Livraison-Marhaba-Frontend.git
+```
+
+#### 1.2. Navigate to the project directory
+
+```sh
+cd Livraison-Marhaba-Frontend
+```
+
+#### 1.3. Install dependencies
+
+```sh
+npm install
+```
+
+### 2. Run the application
+
+```sh
+npm run dev
+```
+
+By default, the application will run on port `3000`.
+
+## File Structure
+
+The project structure follows the **Features    Based** 
+
+### 1. Main Directories
+
+- **src**: This directory contains all the source code of the application.
+
+- **features**: This directory contains all the features folders of the application. Each folder have :
+ - **components**: this directory have all component of the feature.
+ - **layout** : this folder have all layout 
+ - **pages** : pages of the features.
+ - **routes** : This folder have the route of the features.
+
+ ## Technologies using:
+
+- **Front-End**: Reactjs is a frreamwork js engine for interface-side .
+
+- **Back-End**: Node.js and Express.js for the server-side logic.
+
+- **Database** : We use mongoDb database to store user information.
+
+- **User Autorization** : using JWT to autorize the application .
+
+- **Version Control**: Git for code management and collaboration. Docker for Dockrize the application
+
+
--- src/features/products/componets/Cart.jsx
+++ src/features/products/componets/Cart.jsx
@@ -1,19 +1,18 @@
-
-
 import * as React from 'react';
 
 // import  {useEffect} from 'react';
-import  { useEffect } from 'react';
+import { useEffect } from 'react';
 
-import  axios  from 'axios';
+import axios from 'axios';
 
-import { useSelector,useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import {
-    
-    decreaseCart,addToCart,removeFromCart,clearCart,subTotal
- 
-  } from "../redux/cartSlice";
-
+  decreaseCart,
+  addToCart,
+  removeFromCart,
+  clearCart,
+  subTotal,
+} from '../redux/cartSlice';
 
 import Box from '@mui/material/Box';
 import SwipeableDrawer from '@mui/material/SwipeableDrawer';
@@ -27,63 +26,54 @@ import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 
-
 import Swal from 'sweetalert2'; // Import the main SweetAlert2 module
 import 'sweetalert2/dist/sweetalert2.min.css'; // Import the CSS file
 import 'sweetalert2/dist/sweetalert2.min.js'; // Import the JavaScript file
-
+import { selectCurrentUserId } from '../../auth/redux/authSelectors';
 
 export default function SwipeableTemporaryDrawer() {
-
-  const dispatch=useDispatch()
+  const dispatch = useDispatch();
   const cart = useSelector((state) => state.cart);
 
   const handleAddToCart = (product) => {
     dispatch(addToCart(product));
   };
   const handleDecreaseCart = (product) => {
-    dispatch( decreaseCart(product));
+    dispatch(decreaseCart(product));
   };
   const handleRemoveFromCart = (product) => {
-    dispatch( removeFromCart(product));
+    dispatch(removeFromCart(product));
   };
   const handleClearCart = () => {
-    dispatch( clearCart());
+    dispatch(clearCart());
   };
 
-  // const handleAlert = () => {
-  //   Swal.fire({
-  //     title: "Good job!",
-  //     text: "You clicked the button!",
-  //     icon: "success"
-  //   });
-  // };
+ 
+  const userId = useSelector(selectCurrentUserId);
+  console.log(userId)
 
   const handleAlert = async () => {
     try {
-      // Assuming you have access to the user and foods information from your Redux state
       const orderData = {
-        user: "65649f15f7ae2d9095880b74"/* Get user information from your Redux state */,
-        foods: cart.cartItems, // Assuming cart.cartItems contains the list of foods
+        user: userId ,
+        foods: cart.cartItems, 
       };
-  
-      // Make the API call
-      const response = await axios.post('http://localhost:8080/api/order/ordern', orderData);
-  
-      // Handle the response as needed
+
+      
+      const response = await axios.post(
+        'http://localhost:8080/api/order/ordern',
+        orderData
+      );
       console.log('Order placed successfully:', response.data);
-  
-      // Clear the cart after placing the order (you may want to handle this differently based on your requirements)
+
       dispatch(clearCart());
-  
-      // Show a success message or redirect to a success page
+
       Swal.fire({
         title: 'Order Placed!',
         text: 'Thank you for your order.',
         icon: 'success',
       });
     } catch (error) {
-      // Handle errors
       console.error('Error placing order:', error);
       Swal.fire({
         title: 'Error',
@@ -93,15 +83,12 @@ export default function SwipeableTemporaryDrawer() {
     }
   };
 
-
   useEffect(() => {
     dispatch(subTotal());
   }, [cart, dispatch]);
-  
-
 
   const [state, setState] = React.useState({
-    right:false,
+    right: false,
     // top: false,
     // left: false,
     // bottom: false,
@@ -120,7 +107,6 @@ export default function SwipeableTemporaryDrawer() {
   };
 
   const list = (anchor) => (
- 
     <div className="container mx-auto ">
       <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
         <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
@@ -146,48 +132,82 @@ export default function SwipeableTemporaryDrawer() {
           <tbody>
             {Array.isArray(cart.cartItems) &&
               cart.cartItems.map((cartItem) => (
-               <tr key={cartItem.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                  <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                <tr
+                  key={cartItem.id}
+                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+                >
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
                     {cartItem.name}
                   </th>
                   <td className="px-6 py-4">${cartItem.price}</td>
                   <td className="flex px-6 py-4">
-                    <button onClick={() => handleDecreaseCart(cartItem)} className="text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">-</button>
+                    <button
+                      onClick={() => handleDecreaseCart(cartItem)}
+                      className="text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+                    >
+                      -
+                    </button>
                     <span className="text-2xl">{cartItem.quantity}</span>
-                    <button onClick={() => handleAddToCart(cartItem)} className="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">+</button>
+                    <button
+                      onClick={() => handleAddToCart(cartItem)}
+                      className="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+                    >
+                      +
+                    </button>
                   </td>
-                  <td className="px-6 py-4">${(cartItem.price) * cartItem.quantity}</td>
-                  <td className="px-6 py-4"><button onClick={() => handleRemoveFromCart(cartItem)} ><i class="fas fa-trash text-red-500	"></i></button></td>
-
-
+                  <td className="px-6 py-4">
+                    ${cartItem.price * cartItem.quantity}
+                  </td>
+                  <td className="px-6 py-4">
+                    <button onClick={() => handleRemoveFromCart(cartItem)}>
+                      <i class="fas fa-trash text-red-500	"></i>
+                    </button>
+                  </td>
                 </tr>
               ))}
           </tbody>
         </table>
-
       </div>
       {/* <button onClick={() => handleClearCart()} type="button" class=" mt-3 text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Clear Cart</button> */}
 
-      <div className='flex justify-between	'>
-      <button onClick={() => handleClearCart()} type="button" class=" mt-3  text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Clear Cart</button>
-         <div className='mt-3'>
-              <span className='mt-3 text-xl  text-green-500'>SubTotal</span> <span  className='text-xl  text-red-500 mx-3 font-bold	'>${cart.cartTotalAmount}</span>
-         </div>  
-      </div> 
-      <div className='flex justify-center'><button onClick={() => handleAlert()} type="button" class=" mt-6 text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">CheckOut</button></div>
-
-      
- </div>   
-
+      <div className="flex justify-between	">
+        <button
+          onClick={() => handleClearCart()}
+          type="button"
+          class=" mt-3  text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        >
+          Clear Cart
+        </button>
+        <div className="mt-3">
+          <span className="mt-3 text-xl  text-green-500">SubTotal</span>{' '}
+          <span className="text-xl  text-red-500 mx-3 font-bold	">
+            ${cart.cartTotalAmount}
+          </span>
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <button
+          onClick={() => handleAlert()}
+          type="button"
+          class=" mt-6 text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        >
+          CheckOut
+        </button>
+      </div>
+    </div>
   );
 
   return (
     <div>
       {/* {['left', 'right', 'top', 'bottom'].map((anchor) => ( */}
       {['right'].map((anchor) => (
-
         <React.Fragment key={anchor}>
-          <Button onClick={toggleDrawer(anchor, true)}><i className="fas fa-shopping-cart text-3xl "></i></Button>
+          <Button onClick={toggleDrawer(anchor, true)}>
+            <i className="fas fa-shopping-cart text-3xl "></i>
+          </Button>
           <SwipeableDrawer
             anchor={anchor}
             open={state[anchor]}
@@ -198,9 +218,6 @@ export default function SwipeableTemporaryDrawer() {
           </SwipeableDrawer>
         </React.Fragment>
       ))}
-
-
     </div>
   );
 }
-
