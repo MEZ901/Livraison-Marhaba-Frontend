40008a3b14706499f2d8c756ea77c589db56d110
MEZ901 <issammez44@gmail.com>
2023-12-07 15:08:44 +0100

    change localisation manual

--- src/features/deliveryTracking/pages/DeliveryTrackingPage.jsx
+++ src/features/deliveryTracking/pages/DeliveryTrackingPage.jsx
@@ -6,10 +6,56 @@ import { selectCurrentUser } from "../../auth/redux/authSelectors";
 
 const DeliveryTrackingPage = () => {
   const [position, setPosition] = useState([0, 0]);
-  const [customerLocation, setCustomerLocation] = useState([31.6398, -8.0101]);
+  const [customerLocation] = useState([31.6398, -8.0101]);
   const [isLoading, setIsLoading] = useState(true);
   const socket = useSocket();
   const user = useSelector(selectCurrentUser);
+
+  const [index, setIndex] = useState(0);
+
+  const localisations = [
+    [31.6298, -8.0101],
+    [31.6298, -8.0102],
+    [31.6298, -8.0103],
+    [31.6298, -8.0104],
+    [31.6298, -8.0105],
+    [31.6298, -8.0106],
+    [31.6298, -8.0107],
+    [31.6298, -8.0108],
+    [31.6298, -8.0109],
+    [31.6298, -8.011],
+    [31.6298, -8.0111],
+    [31.6298, -8.0112],
+    [31.6298, -8.0113],
+    [31.6298, -8.0114],
+    [31.6298, -8.0115],
+    [31.6298, -8.0116],
+    [31.6298, -8.0117],
+    [31.6298, -8.0118],
+    [31.6298, -8.0119],
+    [31.6298, -8.012],
+    [31.6298, -8.0121],
+    [31.6298, -8.0122],
+    [31.6298, -8.0123],
+    [31.6298, -8.0124],
+    [31.6298, -8.0125],
+    [31.6298, -8.0126],
+    [31.6298, -8.0127],
+    [31.6298, -8.0128],
+    [31.6298, -8.0129],
+    [31.6298, -8.013],
+    [31.6298, -8.0131],
+    [31.6298, -8.0132],
+    [31.6298, -8.0133],
+    [31.6298, -8.0134],
+    [31.6298, -8.0135],
+    [31.6298, -8.0136],
+    [31.6298, -8.0137],
+    [31.6298, -8.0138],
+    [31.6298, -8.0139],
+    [31.6298, -8.014],
+    [31.6298, -8.0141],
+  ];
 
   const updateDeliveryLocation = (position) => {
     try {
@@ -28,7 +74,9 @@ const DeliveryTrackingPage = () => {
         currentPosition.coords.latitude,
         currentPosition.coords.longitude,
       ];
+
       setPosition(newPosition);
+
       updateDeliveryLocation(newPosition);
       setIsLoading(false);
     } catch (error) {
@@ -56,7 +104,14 @@ const DeliveryTrackingPage = () => {
         deliveryId: "656c71c69a4d3371de3f8319",
       });
     }
-  }, [socket, user]);
+  }, [socket]);
+
+  setTimeout(() => {
+    if (index < localisations.length) {
+      setPosition(localisations[index]);
+      setIndex(index + 1);
+    }
+  }, 1000);
 
   useEffect(() => {
     const handleDeliveryLocation = (data) => {
